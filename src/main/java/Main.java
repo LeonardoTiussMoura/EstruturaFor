b9a056d87e31b079bdcbb937f3965fff1125c3ff
LeonardoTiussMoura <167460740+LeonardoTiussMoura@users.noreply.github.com>
2024-09-05 00:12:24 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    //Fazer um programa que lê um valor inteiro N e depois N números inteiros. Ao final mostra a soma dos N números lidos
+
+    for (int i = 0; i < 5; i++  ){
+      System.out.println(i);
+    }
+    System.out.println("");
+    for (int e = 4; e >= 0; e--){
+      System.out.println(e);
+    }
+    sc.close();
+
+    // for ( inicio; condição; incremento ){
+   // comandos
+  //  }
+  }
+}
